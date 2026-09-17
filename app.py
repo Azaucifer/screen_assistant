5e9051c06c132bb8ffd4e09c662b56674827db2f
Syed Shams Junaid <47237500+Azaucifer@users.noreply.github.com>
2026-09-17 06:51:01 +0000

    feat: improve chat interface

--- app.py
+++ app.py
@@ -2,19 +2,35 @@ from dotenv import load_dotenv
 from database import get_messages, init_db, save_message
 from flask import Flask, render_template, request
 from google import genai
+from google.genai import types
+import markdown
 import os
 
 load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-chat = client.chats.create(
-    model="gemini-3.5-flash-lite"
-)
-
 app = Flask(__name__)
 
 init_db()
+
+history = []
+
+for message in get_messages():
+    role = "user" if message[1] == "user" else "model"
+
+    history.append(
+        types.Content(
+            role=role,
+            parts=[types.Part(text=message[2])],
+        )
+    )
+
+
+chat = client.chats.create(
+    model="gemini-3.5-flash-lite",
+    history=history,
+)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -31,9 +47,21 @@ def home():
 
     messages = get_messages()
 
+    formatted_messages = []
+
+    for message in messages:
+        if message[1] == "assistant":
+            content = markdown.markdown(message[2], extensions=["fenced_code"])
+        else:
+            content = message[2]
+
+        formatted_messages.append(
+            (message[0], message[1], content)
+        )
+
     return render_template(
         "index.html",
-        messages=messages,
+        messages=formatted_messages,
     )
 
 
